e4488c3bc73c5246aab714db16c26454356e3032
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-01-16 15:44:35 -0700

    Added keypointIndex for more clarity

--- client/src/motionDetection.js
+++ client/src/motionDetection.js
@@ -1,11 +1,14 @@
+import keypointIndex from './keypointIndex';
+
 const isTouchingShoulder = (pose) => {
     if (pose === false) {
         return false;
     }
-    const leftWristPos = pose.keypoints[9].position;
-    const rightWristPos = pose.keypoints[10].position;
-    const leftShoulderPos = pose.keypoints[5].position;
-    const rightShoulderPos = pose.keypoints[6].position;
+    const leftWristPos = pose.keypoints[keypointIndex.leftWrist].position;
+    const rightWristPos = pose.keypoints[keypointIndex.rightWrist].position;
+    const leftShoulderPos = pose.keypoints[keypointIndex.leftShoulder].position;
+    const rightShoulderPos =
+        pose.keypoints[keypointIndex.rightShoulder].position;
 
     const leftDistance = Math.sqrt(
         Math.pow(leftWristPos.x - leftShoulderPos.x, 2) +
