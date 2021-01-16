cd2881ca7ac372341b244d1af119bc7e3c154155
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-01-16 09:39:47 -0700

    broken posenet code

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,82 @@
-import './App.css';
+// import './App.css';
+import React, { useRef } from 'react';
+import Webcam from 'react-webcam';
+import * as posenet from '@tensorflow-models/posenet';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const webcamRef = useRef(null);
+    const canvasRef = useRef(null);
+
+    //  Load posenet
+    const runPosenet = async () => {
+        const net = await posenet.load({
+            inputResolution: { width: 160, height: 120 },
+            scale: 0.8,
+        });
+        //
+        setInterval(() => {
+            detect(net);
+        }, 30);
+    };
+
+    const detect = async (net) => {
+        if (
+            typeof webcamRef.current !== 'undefined' &&
+            webcamRef.current !== null &&
+            webcamRef.current.video.readyState === 4
+        ) {
+            // Get Video Properties
+            const video = webcamRef.current.video;
+            const videoWidth = webcamRef.current.video.videoWidth;
+            const videoHeight = webcamRef.current.video.videoHeight;
+
+            // Set video width
+            webcamRef.current.video.width = videoWidth;
+            webcamRef.current.video.height = videoHeight;
+
+            // Make Detections
+            const pose = await net.estimateSinglePose(video);
+            console.log(pose);
+        }
+    };
+
+    runPosenet();
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Webcam
+                    ref={webcamRef}
+                    style={{
+                        position: 'absolute',
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                        left: 0,
+                        right: 0,
+                        textAlign: 'center',
+                        zindex: 9,
+                        width: 640,
+                        height: 480,
+                    }}
+                />
+
+                <canvas
+                    ref={canvasRef}
+                    style={{
+                        position: 'absolute',
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                        left: 0,
+                        right: 0,
+                        textAlign: 'center',
+                        zindex: 9,
+                        width: 640,
+                        height: 480,
+                    }}
+                />
+            </header>
+        </div>
+    );
 }
 
 export default App;
