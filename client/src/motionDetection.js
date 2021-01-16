9a50c1d8c604c2cbaa58885022f43dd3d35de90e
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-01-16 16:47:16 -0700

    Added isInAir motion detection

--- client/src/motionDetection.js
+++ client/src/motionDetection.js
@@ -1,31 +1,43 @@
 import keypointIndex from './keypointIndex';
 
-const isTouchingShoulder = (pose) => {
+const isTouchingLeftShoulder = (pose) => {
     if (pose === false) {
         return false;
     }
     const leftWristPos = pose.keypoints[keypointIndex.leftWrist].position;
-    const rightWristPos = pose.keypoints[keypointIndex.rightWrist].position;
     const leftShoulderPos = pose.keypoints[keypointIndex.leftShoulder].position;
-    const rightShoulderPos =
-        pose.keypoints[keypointIndex.rightShoulder].position;
 
     const leftDistance = Math.sqrt(
         Math.pow(leftWristPos.x - leftShoulderPos.x, 2) +
             Math.pow(leftWristPos.y - leftShoulderPos.y, 2)
     );
+
+    return leftDistance < 130 && leftWristPos.y < 200;
+};
+
+const isTouchingRightShoulder = (pose) => {
+    if (pose === false) {
+        return false;
+    }
+    const rightWristPos = pose.keypoints[keypointIndex.rightWrist].position;
+    const rightShoulderPos =
+        pose.keypoints[keypointIndex.rightShoulder].position;
+
     const rightDistance = Math.sqrt(
         Math.pow(rightWristPos.x - rightShoulderPos.x, 2) +
             Math.pow(rightWristPos.y - rightShoulderPos.y, 2)
     );
 
-    if (
-        (leftDistance < 130 && leftWristPos.y < 200) ||
-        (rightDistance < 130 && rightWristPos.y < 200)
-    ) {
-        return true;
-    }
-    return false;
+    return rightDistance < 130 && rightWristPos.y < 200;
 };
 
-export default isTouchingShoulder;
+const isInAir = (pose, normalHeight = 120, peakHeight = -30, factor = 0.75) => {
+    const leftHipPos = pose.keypoints[keypointIndex.leftHip].position;
+    const rightHipPos = pose.keypoints[keypointIndex.rightHip].position;
+    const averageHipHeight = (leftHipPos.y + rightHipPos.y) / 2;
+    const verticalDistance = normalHeight - peakHeight;
+
+    return normalHeight - averageHipHeight > verticalDistance * factor;
+};
+
+export { isTouchingLeftShoulder, isTouchingRightShoulder, isInAir };
