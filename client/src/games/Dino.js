de1b08af7cb34654055c91968c1c4e3febee184f
Jordan Ruetz <jordan.ruetz@mail.utoronto.ca>
2021-01-16 19:04:55 -0500

    dinoooooo

--- client/src/games/Dino.js
+++ client/src/games/Dino.js
@@ -1,303 +1,105 @@
 import { useEffect, useRef } from 'react';
 import isTouchingShoulder from '../../src/motionDetection';
+import useSound from 'use-sound';
+import scoreSfx from './assets/score.mp3'
 
 const Dino = ({ pose, canvasRef, ...props }) => {
+    const [playScore] = useSound(scoreSfx);
+    const bird = useRef(null);
+    const bg = useRef(null);
+    const dino = useRef(null);
+    const cactus = useRef(null);
 
-    //     var game;
-    // var FPS = 60;
-    // var maxScore=0;
+    class Game {
+        constructor() {
+            this.cvs = canvasRef.current;
+            
+            // some variables
+            this.constant = 0;
 
-    // var images = {};
-
-    // var speed = function(fps){
-    // 	FPS = parseInt(fps);
-    // }
-
-    // var loadImages = function(sources, callback){
-    // 	var nb = 0;
-    // 	var loaded = 0;
-    // 	var imgs = {};
-    // 	for(var i in sources){
-    // 		nb++;
-    // 		imgs[i] = new Image();
-    // 		imgs[i].src = sources[i];
-    // 		imgs[i].onload = function(){
-    // 			loaded++;
-    // 			if(loaded == nb){
-    // 				callback(imgs);
-    // 			}
-    // 		}
-    // 	}
-    // }
-
-    // var Bird = function(json){
-    // 	this.x = 80;
-    // 	this.y = 250;
-    // 	this.width = 40;
-    // 	this.height = 30;
-
-    // 	this.alive = true;
-    // 	this.gravity = 0;
-    // 	this.velocity = 0.3;
-    // 	this.jump = -6;
-
-    // 	this.init(json);
-    // }
-
-    // Bird.prototype.init = function(json){
-    // 	for(var i in json){
-    // 		this[i] = json[i];
-    // 	}
-    // }
-
-    // Bird.prototype.flap = function(){
-    // 	this.gravity = this.jump;
-    // }
-
-    // Bird.prototype.update = function(){
-    // 	this.gravity += this.velocity;
-    // 	this.y += this.gravity;
-    // }
-
-    // Bird.prototype.isDead = function(height, pipes){
-    // 	if(this.y >= height || this.y + this.height <= 0){
-    // 		return true;
-    // 	}
-    // 	for(var i in pipes){
-    // 		if(!(
-    // 			this.x > pipes[i].x + pipes[i].width ||
-    // 			this.x + this.width < pipes[i].x ||
-    // 			this.y > pipes[i].y + pipes[i].height ||
-    // 			this.y + this.height < pipes[i].y
-    // 			)){
-    // 			return true;
-    // 	}
-    // }
-    // }
-
-    // var Pipe = function(json){
-    // 	this.x = 0;
-    // 	this.y = 0;
-    // 	this.width = 50;
-    // 	this.height = 40;
-    // 	this.speed = 3;
-
-    // 	this.init(json);
-    // }
-
-    // Pipe.prototype.init = function(json){
-    // 	for(var i in json){
-    // 		this[i] = json[i];
-    // 	}
-    // }
-
-    // Pipe.prototype.update = function(){
-    // 	this.x -= this.speed;
-    // }
-
-    // Pipe.prototype.isOut = function(){
-    // 	if(this.x + this.width < 0){
-    // 		return true;
-    // 	}
-    // }
-
-    // var Game = function(){
-    // 	this.pipes = [];
-    // 	this.birds = [];
-    // 	this.score = 0;
-    // 	this.canvas = document.querySelector("#flappy");
-    // 	this.ctx = this.canvas.getContext("2d");
-    // 	this.width = this.canvas.width;
-    // 	this.height = this.canvas.height;
-    // 	this.spawnInterval = 90;
-    // 	this.interval = 0;
-    // 	this.gen = [];
-    // 	this.alives = 0;
-    // 	this.generation = 0;
-    // 	this.backgroundSpeed = 0.5;
-    // 	this.backgroundx = 0;
-    // 	this.maxScore = 0;
-    // }
-
-    // Game.prototype.start = function(){
-    // 	this.interval = 0;
-    // 	this.score = 0;
-    // 	this.pipes = [];
-    // 	this.birds = [];
-
-    // 	this.gen = Neuvol.nextGeneration();
-    // 	for(var i in this.gen){
-    // 		var b = new Bird();
-    // 		this.birds.push(b)
-    // 	}
-    // 	this.generation++;
-    // 	this.alives = this.birds.length;
-    // }
-
-    // Game.prototype.update = function(){
-    // 	this.backgroundx += this.backgroundSpeed;
-    // 	var nextHoll = 0;
-    // 	if(this.birds.length > 0){
-    // 		for(var i = 0; i < this.pipes.length; i+=2){
-    // 			if(this.pipes[i].x + this.pipes[i].width > this.birds[0].x){
-    // 				nextHoll = this.pipes[i].height/this.height;
-    // 				break;
-    // 			}
-    // 		}
-    // 	}
-
-    // 	for(var i in this.birds){
-    // 		if(this.birds[i].alive){
-
-    // 			var inputs = [
-    // 			this.birds[i].y / this.height,
-    // 			nextHoll
-    // 			];
-
-    // 			var res = this.gen[i].compute(inputs);
-    // 			if(res > 0.5){
-    // 				this.birds[i].flap();
-    // 			}
-
-    // 			this.birds[i].update();
-    // 			if(this.birds[i].isDead(this.height, this.pipes)){
-    // 				this.birds[i].alive = false;
-    // 				this.alives--;
-    // 				//console.log(this.alives);
-    // 				Neuvol.networkScore(this.gen[i], this.score);
-    // 				if(this.isItEnd()){
-    // 					this.start();
-    // 				}
-    // 			}
-    // 		}
-    // 	}
-
-    // 	for(var i = 0; i < this.pipes.length; i++){
-    // 		this.pipes[i].update();
-    // 		if(this.pipes[i].isOut()){
-    // 			this.pipes.splice(i, 1);
-    // 			i--;
-    // 		}
-    // 	}
-
-    // 	if(this.interval == 0){
-    // 		var deltaBord = 50;
-    // 		var pipeHoll = 120;
-    // 		var hollPosition = Math.round(Math.random() * (this.height - deltaBord * 2 - pipeHoll)) +  deltaBord;
-    // 		this.pipes.push(new Pipe({x:this.width, y:0, height:hollPosition}));
-    // 		this.pipes.push(new Pipe({x:this.width, y:hollPosition+pipeHoll, height:this.height}));
-    // 	}
-
-    // 	this.interval++;
-    // 	if(this.interval == this.spawnInterval){
-    // 		this.interval = 0;
-    // 	}
-
-    // 	this.score++;
-    // 	this.maxScore = (this.score > this.maxScore) ? this.score : this.maxScore;
-    // 	var self = this;
-
-    // 	if(FPS == 0){
-    // 		setZeroTimeout(function(){
-    // 			self.update();
-    // 		});
-    // 	}else{
-    // 		setTimeout(function(){
-    // 			self.update();
-    // 		}, 1000/FPS);
-    // 	}
-    // }
-
-    // Game.prototype.isItEnd = function(){
-    // 	for(var i in this.birds){
-    // 		if(this.birds[i].alive){
-    // 			return false;
-    // 		}
-    // 	}
-    // 	return true;
-    // }
-
-    // Game.prototype.display = function(){
-    // 	this.ctx.clearRect(0, 0, this.width, this.height);
-    // 	for(var i = 0; i < Math.ceil(this.width / images.background.width) + 1; i++){
-    // 		this.ctx.drawImage(images.background, i * images.background.width - Math.floor(this.backgroundx%images.background.width), 0)
-    // 	}
-
-    // 	for(var i in this.pipes){
-    // 		if(i%2 == 0){
-    // 			this.ctx.drawImage(images.pipetop, this.pipes[i].x, this.pipes[i].y + this.pipes[i].height - images.pipetop.height, this.pipes[i].width, images.pipetop.height);
-    // 		}else{
-    // 			this.ctx.drawImage(images.pipebottom, this.pipes[i].x, this.pipes[i].y, this.pipes[i].width, images.pipetop.height);
-    // 		}
-    // 	}
-
-    // 	this.ctx.fillStyle = "#FFC600";
-    // 	this.ctx.strokeStyle = "#CE9E00";
-    // 	for(var i in this.birds){
-    // 		if(this.birds[i].alive){
-    // 			this.ctx.save();
-    // 			this.ctx.translate(this.birds[i].x + this.birds[i].width/2, this.birds[i].y + this.birds[i].height/2);
-    // 			this.ctx.rotate(Math.PI/2 * this.birds[i].gravity/20);
-    // 			this.ctx.drawImage(images.bird, -this.birds[i].width/2, -this.birds[i].height/2, this.birds[i].width, this.birds[i].height);
-    // 			this.ctx.restore();
-    // 		}
-    // 	}
-
-    // 	this.ctx.fillStyle = "white";
-    // 	this.ctx.font="20px Oswald, sans-serif";
-    // 	this.ctx.fillText("Score : "+ this.score, 10, 25);
-    // 	this.ctx.fillText("Max Score : "+this.maxScore, 10, 50);
-    // 	this.ctx.fillText("Alive : "+this.alives+" / "+Neuvol.options.population, 10, 100);
-
-    // 	var self = this;
-    // 	requestAnimationFrame(function(){
-    // 		self.display();
-    // 	});
-    // }
-
-    // window.onload = function(){
-    // 	var sprites = {
-    // 		bird:"./img/bird.png",
-    // 		background:"./img/background.png",
-    // 		pipetop:"./img/pipetop.png",
-    // 		pipebottom:"./img/pipebottom.png"
-    // 	}
-
-    // 	var start = function(){
-    // 		Neuvol = new Neuroevolution({
-    // 			population:50,
-    // 			network:[2, [2], 1],
-    // 		});
-    // 		game = new Game();
-    // 		game.start();
-    // 		game.update();
-    // 		game.display();
-    // 	}
-
-    // 	loadImages(sprites, function(imgs){
-    // 		images = imgs;
-    // 		start();
-    // 	})
-
-    // }
+            this.speed = 5;
+            
+            this.bX = 10;
+            
+            this.score = 0;
+            
+            // pipe coordinates 
+            this.obstacles = [];
+        }
+    }
 
     const draw = (ctx) => {
+        const game = canvasRef.current.game;
         const pose = canvasRef.current.pose;
-        if(!pose) return;
-
-
-
-
-        console.log(pose);
+        if(!pose || !game) return;
         ctx.fillStyle = '#000000';
-        const {x, y} = pose.keypoints[0].position;
-        // console.log(ctx.canvas.width, ctx.canvas.height);
-        // ctx.fillRect(x, y, 80, 80);
 
-        let img = new Image()
-        img.src = "assets/dino.png"
+        game.bird = bird.current;
+        game.bg = bg.current;
+        game.dino = dino.current;
+        game.cactus = cactus.current;
 
-        ctx.drawImage(img, 20, 200)
+        const cvWidth = canvasRef.current.width
+        const cvHeight = canvasRef.current.height
+
+        const cactusHeight = cvHeight * 0.2
+        const cactusWidth = cactusHeight * 309 / 533
+
+        const birdHeight = cvHeight * 0.1
+        const birdWidth = birdHeight
+
+        const dinoHeight = 300
+        const dinoWidth = 300
+
+        // ctx.drawImage(game.bg, 0, 0);
+        let max_x = 0
+        for(let i = game.obstacles.length-1; i >= 0; i--) {
+            if (game.obstacles[i].type === "cactus") {
+                ctx.drawImage(game.cactus, game.obstacles[i].x, game.obstacles[i].y, cactusHeight, cactusWidth);
+            } else {
+                ctx.drawImage(game.bird, game.obstacles[i].x, game.obstacles[i].y, birdHeight, birdWidth);
+            }
+
+            if (game.obstacles[i].x > max_x) {
+                max_x = game.obstacles[i].x
+            }
+            
+            game.obstacles[i].x -= game.speed;
+    
+            // detect collision
+            
+            // if( game.bX + game.bird.width >= game.pipe[i].x && game.bX <= game.pipe[i].x + game.pipeNorth.width && (y <= game.pipe[i].y + game.pipeNorth.height || y + game.bird.height >= game.pipe[i].y + game.constant)){
+            //     console.log('fail');
+            // }
+            
+            if(game.obstacles[i].x === 5){
+                game.score++;
+                playScore();
+            }
+
+            if (game.obstacles[i].x < 0) {
+                game.obstacles.splice(i, 1)
+            }
+        }
+
+        if (max_x < 200 ){
+            if (Math.random() < 0.1) {
+                const obsType = Math.random() < 0.7 ? "cactus" : "bird"
+                game.obstacles.push({
+                    x : game.cvs.width,
+                    y : obsType === "cactus" ? cvHeight - cactusHeight : cvHeight * 0.5,
+                    type: obsType
+                })
+            }
+        }
+    
+        // ctx.drawImage(fg, 0, cvs.height - fg.height);
+        
+        ctx.drawImage(dino.current, game.bX, cvHeight - dinoHeight, dinoHeight, dinoWidth);
+        
+        ctx.fillStyle = "#000";
+        ctx.font = "20px Verdana";
+        ctx.fillText("Score : " + game.score, 10, game.cvs.height-20);
     };
 
     const render = () => {
@@ -308,6 +110,7 @@ const Dino = ({ pose, canvasRef, ...props }) => {
     }
 
     useEffect(() => {
+        canvasRef.current.game = new Game();
         render();
     }, []);
 
@@ -318,6 +121,10 @@ const Dino = ({ pose, canvasRef, ...props }) => {
 
     return (
         <>
+            <img ref={bird} src="assets/bird.jpg" style={{display: 'none'}} className="hidden" alt="" />
+            <img ref={bg} src="assets/background.png" style={{display: 'none'}} className="hidden" alt="" />
+            <img ref={cactus} src="assets/cactus.png" style={{display: 'none'}} className="hidden" alt="" />
+            <img ref={dino} src="assets/dino.png" style={{display: 'none'}} className="hidden" alt="" />
         </>
     );
 };
