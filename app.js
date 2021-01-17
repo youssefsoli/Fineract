0fdba17d1a089c81c9cb6f72f6e5d1620cbd9030
youssefsoli <youssefdevelops@gmail.com>
2021-01-17 00:42:35 -0700

    Bring back cooooors

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ const path = require('path');
 const router = require('./routes/');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-      credentials: true
-    }
-});
+const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
