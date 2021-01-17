abada47c2ba73acd4514e8b4c5934ac7c806699b
iokal <iokal@uwo.ca>
2021-01-16 23:54:13 -0500

    Fixed up navbar file

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -2,16 +2,6 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        // <section className="nav_area">
-        // <div className="container">
-        // <div className="logo_container">
-        //     <a href="index.html"
-        //     ><img src="images/logo.png" className="img-fluid" alt="logo" 
-        //     /></a>
-        // </div>
-        // <input type="checkbox" id="toggle" style="display:none;" />
-        // </div>
-        // </section>
         <header className="nav_area">
             <div className="container">
                 <div className="logo_container">
@@ -26,11 +16,6 @@ const Navbar = () => {
                 <nav>
                     <ul>
                         <li><a href="#banner">Home</a></li>
-                        <li><a href="about-us.html">About us</a></li>
-                        <li><a href="our-gallery.html">Our Gallery</a></li>
-                        <li><a href="our-instructor.html">our instructor</a></li>
-                        <li><a href="404-page.html">404 page</a></li>
-                        <li><a href="#contact">Contact us</a></li>
                     </ul>
                 </nav>
          </div>
